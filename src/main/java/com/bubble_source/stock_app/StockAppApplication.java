ec89672927d9269cce6229defb1bc7a1bb956b5c
Jakub Brzezinski <brzezinski.ja@o2.pl>
2018-03-15 19:30:31 +0100

    initial commit

---
+++ src/main/java/com/bubble_source/stock_app/StockAppApplication.java
@@ -0,0 +1,12 @@
+package com.bubble_source.stock_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAppApplication.class, args);
+	}
+}
